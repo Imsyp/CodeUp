2a6419bd0fdf47d890c5ca84603bc089735701b0
Imsyp <tkddud386@gmail.com>
2023-09-30 17:50:53 +0900

    20230930

--- print06.cpp
+++ print06.cpp
@@ -1,15 +1,52 @@
 #include <iostream>
-#include <math.h>
 #include <string>
+#include <math.h>
 using namespace std;
 
 int main(void)
 {
-    float a, b, c;
-    cin>>a;
-    b = modf(a, &c);
-    cout<<c<<endl;
-    cout<<b<<endl;
+    double d, i;
+    cin>>d;
+
+    int n = modf(d, &i);
+
+    string s = to_string(d-i);
+
+    s+=".";
+    string delimiter = ".";
+
+    int pos;
+    int curpos = 0;
+
+    cout<< i <<endl;
+
+    if(s[0] == '-')
+    {
+        s = s.substr(1, s.length()-1);
+    }
+
+    while((pos = s.find(delimiter, curpos)) != string::npos)
+    {
+        int len = pos-curpos;
+        string result = s.substr(curpos, len);
+        if(result.length() != 1)
+        {
+            string new_result;
+            for(int i=0; i<result.length(); i++)
+            {
+                if(result[i] == '0')
+                {
+                    break;
+                }
+
+                new_result += result[i];
+            }
+
+            cout<<new_result<<endl;
+        }
+
+        curpos = pos + 1;
+    }
 
     return 0;
 }
